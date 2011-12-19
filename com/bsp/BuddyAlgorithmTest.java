0f173ab562fec559701a59a15d641311425a5c1b
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2011-12-19 22:02:28 +0100

    extended test. still an allocation error

--- com/bsp/BuddyAlgorithmTest.java
+++ com/bsp/BuddyAlgorithmTest.java
@@ -7,17 +7,31 @@ public class BuddyAlgorithmTest {
 	 */
 	public static void main(String[] args) {
 		BuddyAlgorithm buddy = BuddyAlgorithm.create(128);
+		System.out.println("Allocate 23");
 		buddy.allocate(23);
 		System.out.println(buddy.toString());
+		System.out.println("Allocate 1");
 		buddy.allocate(1);
 		System.out.println(buddy.toString());
+		System.out.println("Allocate 4");
 		buddy.allocate(4);
 		System.out.println(buddy.toString());
+		System.out.println("Allocate 13");
 		buddy.allocate(13);
 		System.out.println(buddy.toString());
+		System.out.println("Allocate 64");
 		buddy.allocate(64);
 		System.out.println(buddy.toString());
+		System.out.println("Allocate 1");
 		buddy.allocate(1);
+		System.out.println(buddy.toString());
+		System.out.println("Reset");
+		buddy = BuddyAlgorithm.create(64);
+		System.out.println("Allocate 32");
+		buddy.allocate(32);
+		System.out.println(buddy.toString());
+		System.out.println("Allocate 16");
+		buddy.allocate(16);
 		System.out.println(buddy.toString());
 	}
 
